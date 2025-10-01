2fcfe075b685c6f3c5b36838983c9d30c197168c
Ahmed Gaballah <ahmed.gaballa81@gmail.com>
2025-10-01 23:30:18 +0300

    Updated company-list.component.ts

--- src/app/components/company-list/company-list.component.ts
+++ src/app/components/company-list/company-list.component.ts
@@ -508,7 +508,7 @@ import { Company, CreateCompanyRequest } from "../../models/company.model";
 
       .company-actions {
         position: absolute;
-        top: 1rem;
+        bottom: 1rem;
         right: 1rem;
         display: flex;
         gap: 0.5rem;
